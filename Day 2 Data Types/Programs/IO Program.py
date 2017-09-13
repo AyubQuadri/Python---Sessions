b4582aeee3a9b0f5e736f12061026b268c145506
Ayub Quadri <ayub.quadri89@gmail.com>
2017-09-14 00:58:20 +0530

    Day 2 Class & Lab activity

---
+++ Day 2 Data Types/Programs/IO Program.py
@@ -0,0 +1,17 @@
+# read the values form users
+
+print 'Enther the value for variable a '
+a = input()
+
+print 'Enter the value for variable b '
+b = input()
+
+# Arithematic Operations
+## Addition
+c = a + b
+print 'Sum = ', c
+
+
+## Substraction
+d = a - b
+print 'Sub = ', d
