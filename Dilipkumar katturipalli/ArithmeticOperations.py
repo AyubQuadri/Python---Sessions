04fe6b1a8102ce7491447b8889559bbc1557c8da
Katturipalli Dilipkumar <katturipalli.dilipkumar@github.com>
2017-09-14 17:26:01 +0100

    Arithmetic Operations
    Updated the arithmetic operations

---
+++ Dilipkumar katturipalli/ArithmeticOperations.py
@@ -0,0 +1,19 @@
+#Arithmetic Operations
+a = 21
+b = 10
+c = 0
+c = a + b #addition operator
+print ("Line 1 - Value of c is ", c)
+
+c=a-b #subtraction operator
+print ("Line 2 - value of c is",c)
+
+c=a%b #modulus operator prints the value of the remainder
+print ("Line 3 - value of C is",c)
+
+c=a/b #divide operation
+print ("Line 4 - Value of C is",c)
+
+c=a*b
+print("Line 5 - Value of C is",c )
+
