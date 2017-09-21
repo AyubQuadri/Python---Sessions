235efb11b595a8d0fffbe3d50f8d3170625fdb43
RAGHU-DESKTOP-M\Raghavender Koltur <kolturra@gmail.com>
2017-09-21 08:54:49 +0530

    Hi,
    Day 2 assignement
    
    Thanks,
    K Raghavender Roa

---
+++ Assignment/Raghavender/Day 2/My Day 2 Training Assignment.py
@@ -0,0 +1,210 @@
+# Assignment 2
+
+'''Below code for 
+question 1'''
+# Logical operations And
+a = True
+b = False
+c = a and b
+print (c)
+
+# Logical operations OR
+a1 = True
+b1 = False
+c1 = a1 or b1
+print(c1)
+
+# Logical operations not equals
+a2 = True
+b2 = False
+c2 = not a2
+print(c2)
+
+# Bitwise operators $
+a3 = 1001
+b3 = 1100
+print(a3 & b3)
+print(bin(a3 & b3))
+
+
+'''Below code is for 
+question 2.a'''
+# Arithmetic operations +
+r = 5
+a = 6
+g = r+a
+print(g)
+# Arithmetic operations -
+r1 = 6
+a1 = 5.5
+g1 = r1-a1
+print(g1)
+# Arithmetic operations *
+r2 = 10
+a2 = 5
+g2 = r2*a2
+print(g2)
+# Arithmetic operations /
+r3 = 6
+a3 = 2
+g3 = r3/a3
+print(g3)
+# Arithmetic operations %
+r4 = 100
+a4 = 70.5
+g4 = a4%r4
+print(g4)
+# Arithmetic operations **
+r5 = 2
+a5 = 6
+g5 = r5**a5
+print(g5)
+
+'''Below code is for
+question 2.b'''
+# Logical And
+t = True
+a = False
+n = t and a
+print(n)
+
+# Logical OR
+t1 = True
+a1 = False
+n1 = t1 or a1
+print(n1)
+
+# Logical Not
+t2 = True
+a2 = False
+n2 = not a2
+print(n2)
+
+
+'''Below code is for
+question 2.c'''
+# Relational operation ==
+f = 2
+f1 = 3
+f2 = f==f1
+print(f2)
+
+# Relational operation !=
+f3 = 2
+f4 = 3
+f5 = f3!=f4
+print(f5)
+
+# Relational operation !=
+f6 = 2
+f7 = 3
+f8 = f6!=f7
+print(f8)
+
+# Relational operation > and >=
+f9 = 2
+f10 = 3
+f11 = f9>f10
+print(f11)
+
+f12 = 6
+f13 = 5
+f14 = f12>=f13
+print(f14)
+
+# Relational operations < and <=
+f15 = 10
+f16 = 15
+f17 = f15<f16
+f18 = f15<=f16
+print(f17)
+print(f18)
+
+
+''''Below code is for 
+question 2.d'''
+
+# Bitwise operations &
+f19 = 4
+f20 = 6
+print(bin(f19))
+print(bin(f20))
+f21 = f19 & f20
+print(f21)
+
+# Bitwise operations |(or)
+f22  = f19|f20
+print(f22)
+# Bitwise operations ^(opposite)
+f23  = f19^f20
+print(f23)
+# Bitwise operations <<(leftshift)
+f24 = f19<<2
+print(f24)
+# Bitwise operations <<(Rightshift)
+f25 = f19>>2
+print(f25)
+
+'''Below code is for Assignment operations 2.e'''
+
+#Assignment operation//  //-> Rounding decimals divident
+
+# Assignment operations for =
+f30 = 10
+f31  = 20
+f31 = f30
+print(f31)
+
+
+# Assignment operation shorthand for addition
+f32=0
+f32+= f31+2
+print(f32)
+
+# Assignment operation  shorthand for subraction
+f33=f32
+f33-= 6
+print(f33)
+
+# Assignment operation  shorthand for mulitplication
+f34 = 2
+f34*= 5
+print(f34)
+
+
+# Assignment operation shorthand for diviion with decimals
+
+f35 = 6
+f35/= 4
+print(f35)
+
+
+# Assignment operation shorthand for precentage
+f36 = 10
+f37 = 5
+f37%=f36
+print(f37)
+
+#Assignment operation shorthand for power multiplaction
+f38 = 5
+f39 = 10
+f39**=f38
+print(f39)
+
+
+#Assignment operation shorthand for division with out decimals
+f40 = 2
+f41 = 9
+f41//=f40
+print(f41)
+
+
+
+
+
+
+
+
+
+
+
