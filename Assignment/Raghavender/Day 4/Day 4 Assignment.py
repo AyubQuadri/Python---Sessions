8e719e80ef0a21855b99e85036cf0be8463d951f
RAGHU-DESKTOP-M\Raghavender Koltur <kolturra@gmail.com>
2017-09-23 18:05:42 +0530

    Hi

---
+++ Assignment/Raghavender/Day 4/Day 4 Assignment.py
@@ -0,0 +1,31 @@
+# below are data types of Numeris
+# Int,Long,Float and Complex
+# int = numbers
+# long like = 19999999999999999999
+#Float  like with decimals 5.2
+#complex is like one is real part number and another is imaginary number
+
+# Boolen data types are true and false'
+# sets are nothing but unordered collections objects like set(10.00, ‘Man’, 12, True, (28+ 9i))
+a =set(10.00,‘Man',12,True,(28+9i))
+a[0]
+
+
+# Below are data types of Sequence
+
+str = 'Hello bubby'# str is defing as variable which is string
+str[0]# this line get string postion, here you will find the zero postion which is H
+
+# list of your data
+mylist = ['Raghu',1,3.5,'Hello']
+mylist
+mylist[0]# this code will get zero poistion list, here which is list 'Raghu'
+mylist[0][1]# this code will get string of zero poistion from 'Raghu' from list
+
+mytuple = ('Surekha','Hello','Presetation','Why',5.55,5,complex(5,8))
+mytuple
+mytuple[0] # this code will get zero poistion list, here which is list 'Raghu'
+mytuple[3][2]# this code will get string of zero poistion from 'Raghu' from list
+
+
+
