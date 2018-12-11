d9e328be80adbd169ffad06fb10369c248924737
Shah Ayub Quadri <ayub.quadri89@gmail.com>
2018-12-11 18:10:48 +0530

    Test Driven Development python
    Simple calculator module for TDD

---
+++ TDD Python/Calculator/app/calculator.py
@@ -0,0 +1,9 @@
+class Calculator(object):
+    def add(self, x, y):
+        number_type = (int, float, complex)
+
+        if isinstance(x, number_type) and isinstance(y, number_type):
+            return x+y
+        else:
+            raise ValueError
+
